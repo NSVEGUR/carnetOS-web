77254f984d53c5cd3c8dbee6616de422ce01e58b
NSVEGUR <nsvegur@gmail.com>
2022-04-09 19:41:23 +0530

    added extra wallpapers

--- src/lib/components/Duplicates/Duplicates.svelte
+++ src/lib/components/Duplicates/Duplicates.svelte
@@ -1,12 +1,12 @@
 <!-- Images to Load Behind the Scenes -->
 
 <img
-	src="/dark-mac-bg.jpeg"
+	src="/wallpapers/monterey-d.jpeg"
 	alt="wall"
 	style="width: 1px; height: 1px; position: fixed; top: 0; right: 0"
 />
 <img
-	src="/light-mac-bg.jpeg"
+	src="/wallpapers/monterey-l.jpeg"
 	alt="wall"
 	style="width: 1px; height: 1px; position: fixed; bottom: 0; left: 0"
 />
