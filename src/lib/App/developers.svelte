7d2c85c7aa5eaf200c27f7443ac885211115f52c
NSVEGUR <nsvegur@gmail.com>
2022-04-17 11:51:22 +0530

    added hogwarts theme

--- src/lib/App/developers.svelte
+++ src/lib/App/developers.svelte
@@ -1,10 +1,42 @@
 <script lang="ts">
+	function showDev(e: any) {
+		const devId = e.target.dataset?.dev;
+		const devAbouts: NodeListOf<HTMLElement> = document.querySelectorAll('.dev-text');
+		devAbouts.forEach((about) => {
+			about.style.display = 'none';
+		});
+		const activeDev: HTMLElement = document.querySelector(`[data-devabout="${devId}"]`);
+
+		activeDev.style.display = 'block';
+	}
 </script>
 
 <div id="backstore" style="display: none">
 	<section id="app-developers">
-		<div>
-			<h1>Developers</h1>
+		<div class="dev-nav">
+			<div data-dev="1" class="dev" on:click={showDev}>Ravi</div>
+			<div data-dev="2" class="dev" on:click={showDev}>Praveen</div>
+			<div data-dev="3" class="dev" on:click={showDev}>Harsha</div>
+		</div>
+		<div class="dev-about">
+			<div data-devabout="1" class="dev-text">
+				Ravi Id voluptate incididunt sit aute exercitation exercitation qui voluptate sint. Sint
+				esse sit officia aute minim sint qui cillum ea ex labore adipisicing. Pariatur ex esse
+				exercitation veniam excepteur aliquip amet. Eiusmod non adipisicing excepteur voluptate
+				occaecat fugiat adipisicing ea dolor tempor ad esse nulla. Cillum enim pariatur elit
+				adipisicing deserunt qui aliqua velit tempor. Voluptate aliqua quis veniam nisi consequat
+				pariatur ea.
+			</div>
+			<div data-devabout="2" class="dev-text">
+				praveen Id id excepteur incididunt exercitation sit. Laborum reprehenderit duis ex
+				incididunt in culpa labore laboris enim. Enim officia reprehenderit aliquip laborum ipsum ut
+				velit ipsum tempor mollit velit.
+			</div>
+			<div data-devabout="3" class="dev-text">
+				Harsha Veniam laboris velit nisi enim in duis. Ea do sunt voluptate laborum. Sunt magna
+				consectetur ut ad. Magna proident tempor sunt consectetur ea labore duis excepteur. Sit
+				voluptate exercitation minim sit laboris pariatur.
+			</div>
 		</div>
 	</section>
 </div>
@@ -14,8 +46,19 @@
 		width: 100%;
 		height: 100%;
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		justify-content: center;
-		align-items: center;
+		align-items: flex-start;
+		gap: 10px;
+		.dev-nav {
+			display: flex;
+			flex-direction: column;
+			width: 30%;
+			height: 100%;
+		}
+		[data-devabout='2'],
+		[data-devabout='3'] {
+			display: none;
+		}
 	}
 </style>
--- src/lib/components/Duplicates/Duplicates.svelte
+++ src/lib/components/Duplicates/Duplicates.svelte
@@ -1,18 +1,18 @@
 <!-- Images to Load Behind the Scenes -->
-<!-- 
+
 <img
-	src="/wallpapers/m1-pro-d.jpeg"
+	src="/wallpapers/train-d.jpeg"
 	alt="wall"
 	style="width: 1px; height: 1px; position: fixed; top: 0; right: 0"
 />
 <img
-	src="/wallpapers/m1-pro-l.jpeg"
+	src="/wallpapers/train-l.jpeg"
 	alt="wall"
 	style="width: 1px; height: 1px; position: fixed; bottom: 0; left: 0"
-/> -->
+/>
 
-<img
+<!-- <img
 	src="/wallpapers/train.jpeg"
 	alt="wall"
 	style="width: 1px; height: 1px; position: fixed; top: 0; right: 0"
-/>
+/> -->
